7c3bf83c4591dc51a12d6527889c5f8aaac57fbe
d1SO <ivansokotuhin1816@gmail.com>
2024-10-11 21:09:41 +0200

    added laba6

--- laba5/laba5.cpp
+++ laba5/laba5.cpp
@@ -6,7 +6,38 @@
 using namespace std;
 
 int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+{	// N1
+	double in = 0;
+	int n = 0;
+	int pl = 0;
+	double mult = 1;
+	double min = numeric_limits<double>::max();
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> in;
+		if (in < 0 && (in - int(in)) < 0) {
+			mult *= in;
+			if (in < min) {
+				pl = i;
+				min = in;
+			}
+		}
+	}
+	cout << endl << mult << endl << min << endl << pl << endl;
+
+
+
+	//// N2
+	//int in;
+	//cin >> in;
+	//int curn;
+	//int min = 9;
+	//while (in > 0) {
+	//	curn = in % 10;
+	//	in = in / 10;
+	//	if (curn < min) {
+	//		min = curn;
+	//	}
+	//}
+	//cout << min;
 }
---
+++ laba6/laba6.cpp
@@ -0,0 +1,21 @@
+﻿// laba6.cpp: определяет точку входа для приложения.
+//
+
+#include "laba6.h"
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[10000];
+	for (int i = 0; i < n; i++) {
+		int inp;
+		cin >> inp;
+		arr[i] = inp;
+
+	}
+	
+
+}
